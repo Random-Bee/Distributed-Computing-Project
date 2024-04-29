f93a960694ef34676a93f48a83c8aed3e518d9db
Vishal Devadiga <vishaldevadiga003@gmail.com>
2024-04-29 22:32:20 +0530

    Connection with vms

--- connect.py
+++ connect.py
@@ -5,7 +5,11 @@ ip_addr = ["33.28", "33.99", "33.251", "32.183"]
 
 def connect():
     for i in range(len(ip_addr)):
-        os.system(f"sshpass -p 'Student1234' ssh ubuntu@10.200.{ip_addr[i]} echo \"1\" &")
+        # os.system(f"sshpass -p 'Student1234' ssh ubuntu@10.200.{ip_addr[i]} 'cd /home/ubuntu/Code/; ./main {i}'")
+        # os.system(f"scp -r ./src ubuntu@10.200.{ip_addr[i]}:/home/ubuntu/Code/")
+        os.system(f"sshpass -p 'Student1234' ssh ubuntu@10.200.{ip_addr[i]} sudo apt install -y openssl-devel")
+        os.system(f"sshpass -p 'Student1234' ssh ubuntu@10.200.{ip_addr[i]} 'cd /home/ubuntu/Code/src; make'")
+        
         
 if __name__ == "__main__":
     connect()
